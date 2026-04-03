09557886829b4045b13e9d5c40e10ed2d89076f6
ImageByLui <lu@imagebylui.com>
2026-04-03 13:20:43 -0400

    Add files via upload

---
+++ src/components/ui/SectionDivider.tsx
@@ -0,0 +1,7 @@
+export default function SectionDivider() {
+  return (
+    <div className="flex justify-center" aria-hidden="true">
+      <div className="w-[60px] h-[1px]" style={{ backgroundColor: "rgba(196,154,92,0.3)" }} />
+    </div>
+  );
+}
